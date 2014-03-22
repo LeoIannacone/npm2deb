4fdf6b520d05a598b9ca630e3577f3601a3a8f6a
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-22 18:08:09 +0100

    pylinted main

--- npm2deb.py
+++ npm2deb.py
@@ -6,33 +6,33 @@ from npm2deb import utils
 import os
 
 def main():
-  usage = 'usage %prog [options] package_name'
-  parser = OptionParser(usage)
-  parser.add_option('-d', '--debug', help='set debug level')
-  opts, args = parser.parse_args()
+    usage = 'usage %prog [options] package_name'
+    parser = OptionParser(usage) 
+    parser.add_option('-d', '--debug', help='set debug level')
+    opts, args = parser.parse_args()
 
-  if len(args) is not 1:
-    parser.error('Please specify a package_name.')
-    exit(1)
+    if len(args) is not 1:
+        parser.error('Please specify a package_name.')
+        exit(1)
 
-  if opts.debug:
-    try:
-      utils.DEBUG_LEVEL = int(opts.debug)
-    except ValueError:
-      print("Error: debug level must be an integer.")
-      exit(1)
+    if opts.debug:
+        try:
+            utils.DEBUG_LEVEL = int(opts.debug)
+        except ValueError:
+            print("Error: debug level must be an integer.")
+            exit(1)
 
-  package_name = args[0]
-  savedPath = os.getcwd()
-  utils.create_dir(package_name)
-  utils.change_dir(package_name)
+    package_name = args[0]
+    saved_path = os.getcwd()
+    utils.create_dir(package_name)
+    utils.change_dir(package_name)
 
-  npm2deb = Npm2Deb(package_name)
-  npm2deb.start()
+    npm2deb = Npm2Deb(package_name)
+    npm2deb.start()
 
-  utils.change_dir(savedPath)
+    utils.change_dir(saved_path)
 
-  print("""
+    print("""
 This is not a crystal ball, so please take a look at auto-generated files.
 This command could help:
 $ grep -r FIX_ME %s/%s/debian/*
