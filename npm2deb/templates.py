3a9e2b7eefd205fe376a2fcbba043d60ebc0d1b3
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-22 18:06:06 +0100

    pylinted other files - added licences.mit

--- npm2deb/templates.py
+++ npm2deb/templates.py
@@ -180,6 +180,26 @@ LICENSE_LGPL_3 = """LGPL-3.0+
  Public License can be found in "/usr/share/common-licenses/LGPL-3".
 """
 
+LICENSE_MIT = """MIT
+ Permission is hereby granted, free of charge, to any person obtaining a
+ copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+ .
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+ .
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 WATCH_GITHUB = """version=3
 opts=filenamemangle=s/.*\/v?([\d\.-]+)\.tar\.gz/%(debian_name)s-$1.tar.gz/ \
  %(homepage)s/tags .*/archive/v?([\d\.]+).tar.gz
