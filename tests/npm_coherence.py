8443d99bcb9dac91bb99eaa33ff834ba96366b87
Leo Iannacone <l3on@ubuntu.com>
2014-04-30 14:08:03 +0200

    better tests structure on debian files

--- tests/npm_coherence.py
+++ tests/npm_coherence.py
@@ -10,6 +10,7 @@ from commands import getstatusoutput
 from shutil import rmtree
 from npm2deb import Npm2Deb, DEBHELPER
 
+
 class npm2deb_fails(unittest.TestCase):
     def test_init_no_npm_module(self):
         try:
@@ -22,13 +23,14 @@ class npm2deb_fails(unittest.TestCase):
 
     def test_init_module_multiple_version(self):
         try:
-            n = Npm2Deb('socket.io@0.x')
+            n = Npm2Deb('socket.io@0.9.x')
             raise Exception
         except Exception as err:
             self.assertTrue(isinstance(err, ValueError))
             # must suggest type of failure
             self.assertTrue(str(err).find(\
                 'More than one version found. Please specify one of:') >= 0)
+
 
 class npm_coherences_license(unittest.TestCase):
 
@@ -59,42 +61,35 @@ class npm_coherences_license(unittest.TestCase):
 
 class debian(unittest.TestCase):
 
-    def test_debhelper_default(self):
+    def _get_compat(self):
+        with open('debian/compat', 'r') as compat_fd:
+            return compat_fd.read().strip('\n')
+
+    def _get_control_field(self, what):
+        result = None
+        with open('debian/control', 'r') as control:
+            for line in control.readlines():
+                if line.find(what) >= 0:
+                    result = line.strip('\n')
+                    break
+        return result
+
+    def test_debhelper(self):
         n = Npm2Deb('parse-url')
         n.create_base_debian()
         n.create_control()
-        debhelper = None
-        with open('debian/control', 'r') as control:
-            for line in control.readlines():
-                if line.find('debhelper') >= 0:
-                    debhelper = line.strip('\n')
-                    break
-        self.assertEqual(debhelper, ' debhelper (>= %s)' % DEBHELPER)
-
-        compat = None
-        with open('debian/compat', 'r') as compat_fd:
-            compat = compat_fd.read().strip('\n')
-        self.assertEqual(compat, str(DEBHELPER))
-
-        rmtree('debian')
+        self.assertEqual(self._get_compat(), str(DEBHELPER))
+        self.assertEqual(self._get_control_field(" debhelper ("),\
+            ' debhelper (>= %s)' % DEBHELPER)
 
     def test_debhelper_as_argument(self):
         MY_DEBHELPER = DEBHELPER + 1
         n = Npm2Deb('parse-url', {'debhelper': MY_DEBHELPER})
         n.create_base_debian()
         n.create_control()
-        debhelper = None
-        with open('debian/control', 'r') as control:
-            for line in control.readlines():
-                if line.find('debhelper') >= 0:
-                    debhelper = line.strip('\n')
-                    break
-        self.assertEqual(debhelper, ' debhelper (>= %s)' % MY_DEBHELPER)
-
-        compat = None
-        with open('debian/compat', 'r') as compat_fd:
-            compat = compat_fd.read().strip('\n')
-        self.assertEqual(compat, str(MY_DEBHELPER))
+        self.assertEqual(self._get_compat(), str(MY_DEBHELPER))
+        self.assertEqual(self._get_control_field(" debhelper ("),\
+            ' debhelper (>= %s)' % MY_DEBHELPER)
 
     def tearDown(self):
         if os.path.isdir('debian'):
