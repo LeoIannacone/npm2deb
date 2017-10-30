80badd843875646796c571b3e95f3187b3c87a22
Raju Devidas <rajudev@disroot.org>
2017-10-31 01:51:55 +0530

    Updated __init__.py - Updated standards and debhelper version.
    The standards and debhelper version were outdated, updating them.

--- npm2deb/__init__.py
+++ npm2deb/__init__.py
@@ -13,8 +13,8 @@ from npm2deb import utils, templates
 from npm2deb.mapper import Mapper
 
 VERSION = '0.2.6'
-DEBHELPER = 9
-STANDARDS_VERSION = '3.9.8'
+DEBHELPER = 10
+STANDARDS_VERSION = '4.1.1'
 
 
 class Npm2Deb(object):
